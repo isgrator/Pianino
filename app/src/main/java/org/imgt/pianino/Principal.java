a5bad1fa2cf92850bef354c174b58a41b85e4efb
Isabel <isgrator@gmail.com>
2017-12-31 22:00:24 +0100

    Primera versión interfaz pantalla principal

--- app/src/main/java/org/imgt/pianino/Principal.java
+++ app/src/main/java/org/imgt/pianino/Principal.java
@@ -1,13 +1,24 @@
 package org.imgt.pianino;
 
+import android.app.Activity;
+import android.graphics.drawable.ShapeDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
-public class Principal extends AppCompatActivity {
+public class Principal extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+
+        ImageView tit_pianino= findViewById(R.id.tit_pianino);
+        Animation apareceTitulo= AnimationUtils.loadAnimation(this, R.anim.anim_tit_pianino);
+        tit_pianino.startAnimation(apareceTitulo);
+
+
     }
 }
